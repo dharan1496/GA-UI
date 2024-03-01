74c1541ca91a2a922d16c4086d1c8cf7e42042e7
dharan1496 <dharan1496@gmail.com>
2024-03-01 22:58:58 +0530

    Date error message update

--- src/app/components/purchases/fibre/fibre-receive-conversion-order/fibre-receive-conversion-order.component.ts
+++ src/app/components/purchases/fibre/fibre-receive-conversion-order/fibre-receive-conversion-order.component.ts
@@ -39,6 +39,8 @@ export class FibreReceiveConversionOrderComponent implements OnInit, OnDestroy {
   subscription = new Subscription();
   updateReceivedCODetails?: ReceiveFibrePO;
   clearSearch = true;
+  maxDate = new Date();
+  dcMaxDate = new Date();
 
   constructor(
     private formBuilder: FormBuilder,
@@ -64,6 +66,10 @@ export class FibreReceiveConversionOrderComponent implements OnInit, OnDestroy {
       recdDate: ['', Validators.required],
       recdDCNo: ['', Validators.required],
       dcDate: ['', Validators.required],
+    });
+
+    this.form.get('recdDate')?.valueChanges.subscribe((value) => {
+      this.dcMaxDate = new Date(value);
     });
 
     if (this.router.url.includes('update-fibre-for-conversion')) {
