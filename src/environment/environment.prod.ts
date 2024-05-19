240737f960c78287ce7cb21a477bd395433c69be
dharan1496 <dharan1496@gmail.com>
2024-05-19 12:41:10 +0530

    updated env prod url

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,7 +1,7 @@
 // To run in IIS server
 export const environment = {
-  baseurl: 'http://localhost:82',
-  api: 'http://localhost:84/api',
+  baseurl: 'http://amman:82',
+  api: 'http://amman:84/api',
   idleTime: 1200, // in seconds (20 mins)
   timeout: 60, // in seconds
 };
