c250e1c4ecda8daab90cadd8dd7d6c2adc18efb4
dharan1496 <dharan1496@gmail.com>
2024-05-22 21:09:12 +0530

    Yarn stock selection in delivery

--- src/app/components/sales/delivery-sales-order/select-yarn-stock/select-yarn-stock.component.ts
+++ src/app/components/sales/delivery-sales-order/select-yarn-stock/select-yarn-stock.component.ts
@@ -1,18 +1,28 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { CommonModule } from '@angular/common';
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Subscription, finalize } from 'rxjs';
 import { MaterialModule } from 'src/app/material.module';
 import { NotifyType } from 'src/app/models/notify';
+import { YarnBlend } from 'src/app/models/yarnBlend';
+import { YarnCounts } from 'src/app/models/yarnCounts';
+import { YarnShade } from 'src/app/models/yarnShade';
 import { YarnStockByOrderId } from 'src/app/models/yarnStockByOrderId';
+import { MasterService } from 'src/app/services/master.service';
 import { YarnService } from 'src/app/services/yarn.service';
 import { NotificationService } from 'src/app/shared/notification.service';
 
 @Component({
   selector: 'app-select-yarn-stock',
   standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
   templateUrl: './select-yarn-stock.component.html',
   styleUrls: ['./select-yarn-stock.component.scss'],
 })
@@ -32,34 +42,87 @@ export class SelectYarnStockComponent implements OnInit, OnDestroy {
   subscription = new Subscription();
   selection = new SelectionModel<YarnStockByOrderId>(true, []);
   loader = false;
+  form!: FormGroup;
+  countsList!: YarnCounts[];
+  shadeList!: YarnShade[];
+  blendList!: YarnBlend[];
 
   constructor(
     private notificationService: NotificationService,
     private matDialogRef: MatDialogRef<any>,
     private yarnService: YarnService,
-    @Inject(MAT_DIALOG_DATA) private orderId: string
+    private formBuilder: FormBuilder,
+    private masterService: MasterService
   ) {}
 
   ngOnInit() {
-    this.loader = true;
+    this.form = this.formBuilder.group({
+      lot: ['', Validators.required],
+      blendId: '',
+      shadeId: '',
+      countsId: '',
+    });
+
     this.subscription.add(
-      this.yarnService
-        .getYarnStockByOrderId(this.orderId)
-        .pipe(finalize(() => (this.loader = false)))
-        .subscribe({
-          next: (stock) => (this.dataSource = stock),
-          error: (error) => {
-            this.dataSource = [];
-            this.notificationService.error(
-              typeof error?.error === 'string' ? error?.error : error?.message
-            );
-          },
-        })
+      this.masterService.getYarnShade().subscribe({
+        next: (data) => (this.shadeList = data),
+        error: (error) => {
+          this.notificationService.error(
+            typeof error?.error === 'string' ? error?.error : error?.message
+          );
+        },
+      })
+    );
+
+    this.subscription.add(
+      this.masterService.getYarnBlend().subscribe({
+        next: (data) => (this.blendList = data),
+        error: (error) => {
+          this.notificationService.error(
+            typeof error?.error === 'string' ? error?.error : error?.message
+          );
+        },
+      })
+    );
+
+    this.subscription.add(
+      this.masterService.getYarnCounts().subscribe({
+        next: (data) => (this.countsList = data),
+        error: (error) => {
+          this.notificationService.error(
+            typeof error?.error === 'string' ? error?.error : error?.message
+          );
+        },
+      })
     );
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  onSearch() {
+    if (this.form.invalid) {
+      this.notificationService.notify(
+        'Please select lot to search',
+        NotifyType.ERROR
+      );
+      return;
+    }
+    this.loader = true;
+    this.selection.clear();
+    this.yarnService
+      .searchYarnStock(this.form.value)
+      .pipe(finalize(() => (this.loader = false)))
+      .subscribe({
+        next: (stock) => (this.dataSource = stock),
+        error: (error) => {
+          this.dataSource = [];
+          this.notificationService.error(
+            typeof error?.error === 'string' ? error?.error : error?.message
+          );
+        },
+      });
   }
 
   isAllSelected() {
@@ -91,5 +154,10 @@ export class SelectYarnStockComponent implements OnInit, OnDestroy {
 
   close() {
     this.matDialogRef.close();
+  }
+
+  onReset() {
+    this.form.reset();
+    this.dataSource = [];
   }
 }
